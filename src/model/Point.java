da0a7d1a237ebdee10991dac41ca30e41577f096
IvI2511 <castaivi@gmail.com>
2025-05-18 18:13:39 +0200

    Point en model

---
+++ src/model/Point.java
@@ -0,0 +1,11 @@
+package model;
+import java.io.Serializable;
+
+public class Point implements Serializable {
+    public int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
