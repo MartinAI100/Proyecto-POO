622ec864127925893ff62ff0125bda05d2bdaf0c
IvI2511 <castaivi@gmail.com>
2025-05-18 18:31:38 +0200

    arreglo cuadrado

--- src/model/Cuadrado.java
+++ src/model/Cuadrado.java
@@ -1,6 +1,17 @@
-@Override
-public boolean contiene(int x, int y) {
-    int half = lado / 2;
-    return x >= posicion.x - half && x <= posicion.x + half &&
-           y >= posicion.y - half && y <= posicion.y + half;
+package model;
+import java.awt.*;
+
+public class Cuadrado extends Figura {
+    private int lado;
+
+    public Cuadrado(Point posicion, Color color, int lado) {
+        super(posicion, color);
+        this.lado = lado;
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        g.setColor(color);
+        g.fillRect(posicion.x - lado/2, posicion.y - lado/2, lado, lado);
+    }
 }
